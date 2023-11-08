4ac2230c763d0fb2461a480683abfc05f0929704
all10burgers <travisalten@gmail.com>
2023-11-08 10:30:21 -0600

    fix

--- lab12/index.php
+++ lab12/index.php
@@ -4,7 +4,7 @@ if(isset($_COOKIE['cs4413'])) {
     echo "Cookie 'cs4413' is already set with the value: " . $_COOKIE['cs4413'];
 } else {
     // Set the cookie with your desired value and expiration time
-    $cookieValue = "your_desired_value";
+    $cookieValue = "HELLO";
     $expirationTime = time() + 60 * 60 * 24 * 7; // 1 week (adjust as needed)
 
     setcookie('cs4413', $cookieValue, $expirationTime);
